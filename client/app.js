f2fc48d73a045ab1d6231e67aec5f19329fb1d65
izafar22 <izafar22@gmail.com>
2016-09-29 03:46:51 +0530

    done

--- client/app.js
+++ client/app.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
+   
+        $(document).mousemove(function(event){
+        $("span").text("X: " + event.pageX + ", Y: " + event.pageY);
+    });
 
+    
+    
     $('#point1').css('background-color','green');
 
     
